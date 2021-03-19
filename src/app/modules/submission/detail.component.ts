8d5a15f1b1162e84e43186d5c97a654ae1e5842e
pgirala <pgirala@gmail.com>
2021-03-19 19:54:55 +0100

    URL de prueba.

--- src/app/modules/submission/detail.component.ts
+++ src/app/modules/submission/detail.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-detail',
-  template: '<formio src="https://examples.form.io/example"></formio>'
+  template: '<formio src="http://localhost:3001/mrr/medida"></formio>'
 })
 export class DetailComponent {
 }
