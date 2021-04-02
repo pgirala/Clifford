c5bff00f8c3d156ca1ab785ab60c58a318b9a381
pgirala <pgirala@gmail.com>
2021-04-03 00:28:44 +0200

    Se intenta eliminar el mensaje de confirmación.

--- src/app/modules/submission/view/detail.component.ts
+++ src/app/modules/submission/view/detail.component.ts
@@ -22,7 +22,10 @@ export class DetailComponent implements OnInit{
       this.readOnly = (data.action == 'view');
       this.renderOptions = {
         language: 'sp',
-        i18n: CONSTANTS.i18n
+        i18n: CONSTANTS.i18n,
+        submitMessage: "Acción realizada",
+        disableAlerts: true,
+        noAlerts: true
       };
   }
 
