1a93c92490f41243e9d97ae36a278de32ea3aea6
pgirala <pgirala@gmail.com>
2021-08-09 03:52:16 +0200

    En la prueba espera a que la aplicación se cargue.

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,3 +1,4 @@
+import { element } from 'protractor';
 import { AppPage } from './app.po';
 
 describe('Clifford', () => {
@@ -12,7 +13,16 @@ describe('Clifford', () => {
 	page.navigateTo();
     browser.wait(ExpectedConditions.visibilityOf(browser.element(by.id('kc-login'))), 10000)
       .then(() => {
-            expect(element(by.id('kc-login')).isPresent()).toBe(true);
+          let boton = element(by.id('kc-login'));
+          expect(boton.isPresent()).toBe(true);
+          element(by.id('username')).sendKeys('pgirala');
+          element(by.id('password')).sendKeys('Test@2020');
+          boton.click();
+          browser.wait(ExpectedConditions.visibilityOf(browser.element(by.cssContainingText('*', 'Seleccione la agrupación de formularios'))), 10000)
+            .then(() => {
+            browser.ignoreSynchronization = false;
+            expect(element(by.cssContainingText('*', 'Pgirala / Oficina de Informática Presupuestaria')).isPresent()).toBeTruthy();
+          });
     });
   });
 });
