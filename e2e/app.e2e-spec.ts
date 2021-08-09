9e829c586b19c9506006272629ee0961d6dfd653
pgirala <pgirala@gmail.com>
2021-08-09 18:33:32 +0200

    Refactoring.

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -8,22 +8,8 @@ describe('Clifford', () => {
     page = new AppPage();
   });
 
-  it('Login de Keycloak', () => {
-	browser.ignoreSynchronization = true;
-	page.navigateTo();
-    browser.wait(ExpectedConditions.visibilityOf(browser.element(by.id('kc-login'))), 10000)
-      .then(() => {
-          let boton = element(by.id('kc-login'));
-          expect(boton.isPresent()).toBe(true);
-          element(by.id('username')).sendKeys('pgirala');
-          element(by.id('password')).sendKeys('Test@2020');
-          boton.click();
-          browser.wait(ExpectedConditions.visibilityOf(browser.element(by.cssContainingText('*', 'Seleccione la agrupación de formularios'))), 10000)
-            .then(() => {
-            browser.ignoreSynchronization = false;
-            expect(element(by.cssContainingText('*', 'Pgirala / Oficina de Informática Presupuestaria')).isPresent()).toBeTruthy();
-          });
-    });
+  it('permite crear dominios', () => {
+    page.loginAsAdminitrator();
   });
 });
 
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -8,4 +8,22 @@ export class AppPage {
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  loginAsAdminitrator() {
+    browser.ignoreSynchronization = true;
+    this.navigateTo();
+    browser.wait(ExpectedConditions.visibilityOf(browser.element(by.id('kc-login'))), 10000)
+      .then(() => {
+          let boton = element(by.id('kc-login'));
+          expect(boton.isPresent()).toBe(true);
+          element(by.id('username')).sendKeys('pgirala');
+          element(by.id('password')).sendKeys('Test@2020');
+          boton.click();
+          browser.wait(ExpectedConditions.visibilityOf(browser.element(by.cssContainingText('*', 'Seleccione la agrupación de formularios'))), 10000)
+            .then(() => {
+            browser.ignoreSynchronization = false;
+            expect(element(by.cssContainingText('*', 'Pgirala / Oficina de Informática Presupuestaria')).isPresent()).toBeTruthy();
+          });
+      });
+  }
 }
